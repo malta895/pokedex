92e2b7347bd1724f1c78de5d6be11751d0500316
malta895 <lmalta95@gmail.com>
2024-05-30 21:32:29 +0200

    feat: first green light impl

--- pokemon/pokemon.go
+++ pokemon/pokemon.go
@@ -1,5 +1,14 @@
 package pokemon
 
+import (
+	"io"
+	"net/http"
+)
+
+type PokemonClient struct {
+	baseUrl string
+}
+
 type Pokemon struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
@@ -7,7 +16,22 @@ type Pokemon struct {
 	IsLegendary bool   `json:"isLegendary"`
 }
 
-func PokemonByName(name string) (Pokemon, error) {
+func NewClient(baseUrl string) PokemonClient {
+	return PokemonClient{baseUrl}
+}
+
+func (p PokemonClient) PokemonByName(name string) (Pokemon, error) {
+	resp, err := http.Get(p.baseUrl + "/" + "ditto")
+	if err != nil {
+		return Pokemon{}, err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
 	return Pokemon{
 		Name:        "ditto",
 		Description: "Capable of copying\nan enemy's genetic\ncode to instantly\ftransform itself\ninto a duplicate\nof the enemy.",
