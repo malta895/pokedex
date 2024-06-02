57adb306ddf2480bdced150cc6dbc91f99e45a67
malta895 <lmalta95@gmail.com>
2024-06-02 19:02:13 +0200

    docs: funtranslations

---
+++ apiclients/funtranslations/doc.go
@@ -0,0 +1,3 @@
+// Package funtranslations provides a client for the Fun Translations API.
+// It also provides a way to map the API response to the application data structures.
+package funtranslations
